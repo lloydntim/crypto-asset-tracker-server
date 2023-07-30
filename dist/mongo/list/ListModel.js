77127258ce5a208b237d40f1be8896a96f721663
Lloyd Ntim <lloyd.ntim@gmail.com>
2023-07-30 16:52:44 +0000

    Removed .env file

--- dist/mongo/list/ListModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
-const ListSchema_1 = __importDefault(require("./ListSchema"));
-exports.default = mongoose_1.default.model('List', ListSchema_1.default);
-//# sourceMappingURL=ListModel.js.map
