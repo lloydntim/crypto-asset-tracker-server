817e0145556775bc23666834e56d51766803f157
Lloyd Ntim <lloyd.ntim@gmail.com>
2023-08-19 12:13:17 +0000

    updated localisation config

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,25 +1,21 @@
-
-
 import i18next from 'i18next';
 import { LanguageDetector } from 'i18next-http-middleware';
 
 import translationEn from './locales/en/translation.json';
 import translationDe from './locales/de/translation.json';
 
-i18next
-  .use(LanguageDetector)
-  .init({
-    resources: {
-      en: {
-        translation: translationEn,
-      },
-      de: {
-        translation: translationDe,
-      },
+i18next.use(LanguageDetector).init({
+  resources: {
+    en: {
+      translation: translationEn,
     },
-    fallbackLng: 'en',
-    preload: ['en', 'de'],
-    saveMissing: true
-  });
+    de: {
+      translation: translationDe,
+    },
+  },
+  fallbackLng: 'en',
+  preload: ['en', 'de'],
+  saveMissing: true,
+});
 
 export default i18next;
